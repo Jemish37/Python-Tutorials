936444f64510737678afcc78766deee4c272825f
Jemish Dhameliya <jemish268@gmail.com>
2022-12-16 23:02:21 +0530

    Two new files added

---
+++ 8_String_OPs_Slicings.py
@@ -0,0 +1,20 @@
+str = "Hello, yes I'm wanted man"
+str2 = "Mogambo khush hua"
+
+print(len(str))
+
+print(str[1])
+print(str[0:5])
+print(str[1:6])
+print(str[15:21])
+print(str[7:])
+print(str2[8:])
+ 
+str3 = "abcdq werzxc vmnb vpoiu"
+
+print(len(str3))
+print(str3[0:-3])
+print(str3[0:len(str3)-3])
+print(str3[-8:len(str3)-2])
+print(str3[len(str3)-8:len(str3)-2])
+
---
+++ 9_String_Methods.py
@@ -0,0 +1,73 @@
+# Strings are Immutables, value of string can't be change 
+
+
+
+# str1 = "jemish is the next gdenius developed."
+# str2 = "jemish12fdgfdg"
+# str3 = "13 gdfgf bvnjbnv dgdfgdf 12"
+
+# print(str1.upper())
+# print(str1.lower())
+# print(str1.center(5)) # don't have idea
+# print(str1.capitalize())
+# print(str1.casefold())
+# print(str1.count("g"))
+# print(str1.endswith("."))
+# # print(str1.index("next"))
+# print(str1.find("i12s"))
+# print(str2.isalnum())
+# print(str3)
+# print(str3.strip())
+# print(str3.rstrip("dgdfgdf 12"))
+# print(str3.lstrip("dgdfgdf 12"))
+# print(str1.replace("gdenius", "genius"))
+
+# str1 = str1.replace("gdenius", "genius")
+# print(str1.replace("developed", "developer"))
+
+
+# str4 = "jemish145452@gmail.com,jd343emish12@gmail.com, jem4324ish12@gmail.com,j42332emish14353452@gmail.com,jemish143542@gmail.com,jemish143545, 2@gmail.com,jemish1435342@gmail.com"
+
+# print(str4.split(","))
+# print(str4.split(", "))
+
+# str5 = "dfdg dgydfyb yfyfdgbyf dgfydgbyf dgyfbg ydfgiybg iybfdygbfdyigbdfygfgbfdyg dfgbdfiygbdfiybdfgdf fdiygbdfgfdybgvdfiuybgfd cxvcvvcvvcvcvcvcvv gidfuyghdfiughfouadosd gdfsougsudfgsdufgg gdfsgs dfgsfgdsg"
+
+# lst = str5.split(" ")
+
+# print(lst)
+# print(lst[1][3])
+
+# str6 = "Hello"
+# print(str6.center(10))
+
+# print(len(str6.center(80)))
+
+
+# str7 = "this is new world"
+
+# print(str7.endswith("world"))
+# print(str7.endswith("new",5,11))
+
+# use startswith() also 
+# name = "Jemish12"
+# name2 = "12"
+# print(name.isalnum())
+# print(name.isalpha())
+# print(name2.isnumeric())
+# print(name.islower())
+# print(name.isupper())
+
+# str8 = "this is unprintable \n"
+# print(str8.isprintable())
+
+str9 = "    "
+print(str9.isspace())
+
+str10 = "dfggDSFGDGDfdfgfdDFGFG"
+
+print(str10.swapcase())
+
+str11 = "this is a title you nonsense"
+
+print(str11.title())
